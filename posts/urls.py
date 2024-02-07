1afb818cd8fe06b93b099c101e7526fe72a03f5b
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-02-07 16:25:51 +0000

    commit

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from posts import views
 # PostList is a class view so remember to call as_view on it
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]
