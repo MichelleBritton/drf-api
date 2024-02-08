f5d66c0c9bdb6c0973f609817ca66603cc38dbe1
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-02-08 15:54:13 +0000

    commit

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+# List and create likes so extend teh ListCreateAPIView generics class
+# we have to make sure only the authenticated users can like posts by setting permission classes
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    #Just like when we set the user creating a comment as it's owner, we'll do teh same here iwth the Like
+    # When saving the like instances to our database, we'll set the owner to be the user making the request
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
